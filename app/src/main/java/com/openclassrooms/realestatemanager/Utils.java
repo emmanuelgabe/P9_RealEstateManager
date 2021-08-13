f0858320f7b790fe19b3d113228eb02e746c21ee
emmanuelgabe <47250716+emmanuelgabe@users.noreply.github.com>
2021-08-13 11:57:03 +0200

    improvement of the Utils class with its tests

--- app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
@@ -1,11 +1,16 @@
 package com.openclassrooms.realestatemanager;
 
 import android.content.Context;
-import android.net.wifi.WifiManager;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Philippe on 21/02/2018.
@@ -13,34 +18,30 @@ import java.util.Date;
 
 public class Utils {
 
-    /**
-     * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
-     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
-     * @param dollars
-     * @return
-     */
-    public static int convertDollarToEuro(int dollars){
-        return (int) Math.round(dollars * 0.812);
+    public static int convertDollarToEuro(int dollars) {
+        return (int) Math.round(dollars * 0.844);
     }
 
-    /**
-     * Conversion de la date d'aujourd'hui en un format plus approprié
-     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
-     * @return
-     */
-    public static String getTodayDate(){
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    public static int convertEurosToDollar(int euros) {
+        return (int) Math.round(euros * 1.186);
+    }
+
+    public static String getTodayDate() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
         return dateFormat.format(new Date());
     }
 
-    /**
-     * Vérification de la connexion réseau
-     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
-     * @param context
-     * @return
-     */
-    public static Boolean isInternetAvailable(Context context){
-        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
-        return wifi.isWifiEnabled();
+    public static Boolean isInternetAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Network network = connectivityManager.getActiveNetwork();
+            if (network == null) return false;
+            NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+            return networkCapabilities != null && (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR));
+        } else {
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            return networkInfo != null && networkInfo.isConnected();
+        }
     }
 }
---
+++ app/src/test/java/com/openclassrooms/realestatemanager/UtilsUnitTest.kt
@@ -0,0 +1,68 @@
+package com.openclassrooms.realestatemanager
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build.VERSION
+import androidx.test.core.app.ApplicationProvider
+import com.openclassrooms.realestatemanager.Utils.*
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
+import org.robolectric.annotation.Config
+import org.robolectric.util.ReflectionHelpers
+import java.util.*
+
+
+/**
+ * Create by Emmanuel gabé on 12/08/2021.
+ */
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [29])
+class UtilsUnitTest {
+
+    @Test
+    fun eurosToDollar_returnCorrectConversion() {
+        val dollar = 1000
+        val euros = convertDollarToEuro(dollar)
+        assertEquals(844, euros)
+    }
+
+    @Test
+    fun dollarToEuros_returnCorrectConversion() {
+        val euros = 100
+        val dollar = convertEurosToDollar(euros)
+        assertEquals(119, dollar)
+    }
+
+    @Test
+    fun getDayToDate_returnCorrectDateWithRightFormat() {
+        val currentDate = getTodayDate().split("/")
+        assertEquals(Calendar.getInstance().get(Calendar.DAY_OF_MONTH), currentDate[0].toInt())
+        assertEquals(Calendar.getInstance().get(Calendar.MONTH) + 1, currentDate[1].toInt())
+        assertEquals(Calendar.getInstance().get(Calendar.YEAR), currentDate[2].toInt())
+
+    }
+
+    @Test
+    fun getInternetAvailability_returnCorrectState() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        Assert.assertTrue(isInternetAvailable(context))
+        val shadowOfActiveCapabilities = Shadows.shadowOf(connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork))
+        shadowOfActiveCapabilities.removeTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        shadowOfActiveCapabilities.removeTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+        Assert.assertFalse(isInternetAvailable(context))
+
+        ReflectionHelpers.setStaticField(VERSION::class.java, "SDK_INT", 19)
+        val shadowOfActiveNetworkInfo = Shadows.shadowOf(connectivityManager.activeNetworkInfo)
+        shadowOfActiveNetworkInfo.setConnectionStatus(NetworkInfo.State.CONNECTED)
+        Assert.assertTrue(isInternetAvailable(context))
+    }
+}
